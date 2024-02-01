507babce5cff5b730d22ac8f6461af0caa7ffe2e
johannes-kuhfuss <johannes.kuhfuss@web.de>
2024-02-01 07:46:23 +0100

    more unit testing

--- dto/Config_test.go
+++ dto/Config_test.go
@@ -1,6 +1,7 @@
 package dto
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/johannes-kuhfuss/radio-stats/config"
@@ -46,9 +47,12 @@ func Test_GetConfig_NoGpios_Returns_NoError(t *testing.T) {
 func Test_GetConfig_WithGpios_Returns_NoError(t *testing.T) {
 	config.InitConfig("", &testConfig)
 	var dec config.PinConfigDecoder
-	var teststring = "1={\"name\":\"SD1 Master Alarm\",\"invert\": true};20={\"name\":\"SD1 Aux Alarm\",\"invert\":false}"
+	var teststring = "1={\"name\":\"SD1 Master Alarm\",\"invert\": true};20={\"name\":\"SD1 Aux Alarm\",\"invert\":false};40={\"name\":\"KS9\",\"invert\":false}"
 	dec.Decode(teststring)
 	testConfig.Gpio.InConfig = dec
+	testConfig.Gpio.OutConfig = make(map[string]int)
+	testConfig.Gpio.OutConfig["KS1"] = 1
+	testConfig.Gpio.OutConfig["KS2"] = 2
 	config.SetupGpios(&testConfig)
 
 	resp := GetConfig(&testConfig)
@@ -58,4 +62,20 @@ func Test_GetConfig_WithGpios_Returns_NoError(t *testing.T) {
 	assert.EqualValues(t, "SD1 Master Alarm", resp.GpioPins[0].Name)
 	assert.EqualValues(t, "Inverted", resp.GpioPins[0].Invert)
 	assert.EqualValues(t, "Non inverted", resp.GpioPins[1].Invert)
+	assert.EqualValues(t, "KS9", resp.KsPins[0].Name)
+	assert.EqualValues(t, "KS1 KS2", strings.Join(resp.GpioOuts, " "))
+}
+
+func Test_GetConfig_WithGpioOut_returnsNoError(t *testing.T) {
+
+}
+
+func Test_volumeString_ReturnsString(t *testing.T) {
+	vol := make(map[string]float64)
+	vol["test1"] = 1.1
+	vol["test2"] = 2.2
+
+	volStr := volumeString(vol)
+
+	assert.EqualValues(t, "test1=1.1 # test2=2.2 # ", volStr)
 }
