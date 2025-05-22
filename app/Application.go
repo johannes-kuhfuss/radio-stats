38d86f0e316198d8e532e2e6da4de8efa3fe3a93
johannes-kuhfuss <johannes.kuhfuss@web.de>
2025-05-22 20:29:44 +0200

    Secure ciphers

--- app/Application.go
+++ app/Application.go
@@ -97,15 +97,13 @@ func initServer() {
 
 	if cfg.Server.UseTls {
 		tlsConfig = tls.Config{
-			CipherSuites: []uint16{
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-			},
 			PreferServerCipherSuites: true,
-			MinVersion:               tls.VersionTLS12,
-			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+			MinVersion:               tls.VersionTLS13,
+			CurvePreferences: []tls.CurveID{
+				tls.X25519,
+				tls.CurveP256,
+				tls.CurveP384,
+			},
 		}
 	}
 	if cfg.Server.UseTls {
