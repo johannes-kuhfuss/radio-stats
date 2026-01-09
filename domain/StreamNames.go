aa27afe9418504b64b46cf1f8eac1d5d4e067c33
johannes-kuhfuss <johannes.kuhfuss@web.de>
2026-01-09 19:13:03 +0100

    adjusted stream names

--- domain/StreamNames.go
+++ domain/StreamNames.go
@@ -7,6 +7,7 @@ var (
 		"http://streaming.fueralle.org:80/coloradio_160.mp3": "MP3 - 160kBit/s",
 		"http://streaming.fueralle.org:80/coloradio_160.ogg": "Ogg - 160kBit/s",
 		"http://streaming.fueralle.org:80/coloradio_48.aac":  "AAC - 48kBit/s",
+		"http://streaming.fueralle.org:80/coloradio_48.opus": "Opus - 48kBit/s",
 		"http://streaming.fueralle.org:80/coloradio_56.mp3":  "MP3 - 56kBit/s",
 		"http://streaming.fueralle.org:80/coloradio_hq":      "Contribution Stream",
 	}
